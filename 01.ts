8407748e5459b64ef75c6be786edec299be79841
Ianis <janis.sci@gmail.com>
2019-10-30 14:05:13 +0100

    First lesson

---
+++ 01.ts
@@ -0,0 +1,33 @@
+// #########################
+// ~~~ Learn TypeScript ~~~
+//    01. BASIC TYPES
+// #########################
+
+// Implicit `any`
+// let u = 3
+// u = 123.123
+
+// Basic types:
+// let b: boolean = true
+// let s: string = "string"
+// let n: number = 123.3
+
+// Array types, array of strings
+// let arr: number[] = []
+// arr.push(123)
+// arr.push('string')
+
+// Tuples: 
+// let tuple: [string, number, number]
+// tuple = ["123", 1, "string"]
+
+// Special types: undefined and null
+// let und: undefined = undefined
+// let nu: null = null
+
+// Type unions
+// let v: string = "string"
+// v = null
+
+// Never
+// let a: never = 123
