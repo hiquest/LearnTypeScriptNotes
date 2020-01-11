529b114817e647075a69bb0ef8a6047fc22e9f8a
Ianis <janis.sci@gmail.com>
2020-01-11 13:54:11 +0100

    Section 06

--- 06-compiler-options.ts
+++ 06-compiler-options.ts
@@ -3,10 +3,100 @@
 // 06. Compiler Options
 // #########################
 
-// https://www.typescriptlang.org/docs/handbook/compiler-options.html
+// Where to look for compiler arguments: https://www.typescriptlang.org/docs/handbook/compiler-options.html
 
-// --declarations
+// 💡 always aim for a stricter setup. New poject: make it as strict as possible, legacy projects: start with vague and then adjust, and make it stricter.
 
-function tellme(name) {
-  return name.xxx
-}
+// #################
+// 💡 Strict checks
+// #################
+//                                     --|
+// --noImplicitAny                       |
+// --strictNullChecks                    |
+// --strictBindCallApply                 |
+// --alwaysStrict                        | => --strict
+// --strictFunctionTypes                 |
+// --strictPropertyInitialization        |
+// --noImplicitThis                      |
+//                                     --|
+// --noImplicitReturns
+// --noFallthroughCasesInSwitch
+// --noUnusedLocals
+// --noUnusedParameters
+
+// --noImplicitAny (since 2.0)
+// function hello(x: []) {
+//   console.log(x.length)
+// }
+// hello(123)
+
+// --strictNullChecks (since 2.6)
+// More info: https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-0.html#example
+// let x: string | null = null
+
+// --strictBindCallApply (since 3.2)
+// function foo(a: number, b: string): string {
+//     return a + b;
+// }
+// foo('string', 'string')
+// foo.apply(null, ['string', 'string'])
+
+// --alwaysStrict parses in strict mode and emits "use strict"
+// More info: https://www.w3schools.com/js/js_strict.asp
+
+// --strictFunctionTypes
+// interface User {
+//     name: string
+// }
+// interface UserWithEmail extends User {
+//     email: string
+// }
+
+// function fetchUser(onSuccess: (user: User) => void) {}
+// fetchUser((u: UserWithEmail) => { u.email })
+  
+// --strictPropertyInitialization (since 2.7)
+// class User {
+//     name: string
+//     constructor(name) {
+//         this.name = name
+//     }
+// }
+// const user = new User('John')
+// user.name.split(",")
+
+// --noImplicitThis
+// class Foo {
+//     x = 5
+//     bar() {
+//         return () => {
+//             console.log(this.x)
+//         }
+//     }
+// }
+
+// --noImplicitReturns
+// function hello(name: string | null) {
+//   if (name) {
+//     return "Hello, " + name
+//   }
+// }
+
+// --noFallthroughCasesInSwitch
+// function sizeToPrice(size: 's' | 'm' | 'l') {
+//     let price = 15
+//     switch(size) {
+//         case 's':
+//             price = 10
+//         case 'm':
+//             price = 11
+//             break;
+//     }
+//     return price
+// }
+
+// --noUnusedLocals & --noUnusedParameters
+// function test(x: string, y: number) {
+//     const test = y * 5
+//     console.log('x', x)
+// }
